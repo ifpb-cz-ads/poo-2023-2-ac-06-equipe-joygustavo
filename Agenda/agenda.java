016c059d0fe504acfa7a332e3b297850a162c562
Joy Carvalho <94726085+JoyceeCarvalho@users.noreply.github.com>
2023-12-08 22:35:03 -0300

    Agenda main

--- Agenda/agenda.java
+++ Agenda/agenda.java
@@ -1,52 +1,52 @@
-package model;
-
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
-public class Agenda implements Serializable {
+public class agenda<Contato> implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private List<Contato> contatos;
 
-    public Agenda(Usuario usuario){
+    public <Usuario> agenda(Usuario usuario) {
         contatos = new ArrayList<>();
     }
 
-    //Adicionar contato à agenda - CREATE
-    public boolean adicionar(Contato contato){
-        return contatos.contains(contato)?false:contatos.add(contato);
+    // Adicionar contato à agenda - CREATE
+    public boolean adicionar(Contato contato) {
+        return contatos.contains(contato) ? false : contatos.add(contato);
     }
 
-    //Listar os contatos - READ
+    // Listar os contatos - READ
     public List<Contato> getContatos() {
         Collections.sort(contatos);
         return contatos;
     }
 
-    //Atualizar um contato - UPDATE
-    public boolean atualizar(Contato atual, Contato novo){
-        int posicao =contatos.indexOf(atual);
-        if(posicao>=0){
+    // Atualizar um contato - UPDATE
+    public boolean atualizar(Contato atual, Contato novo) {
+        int posicao = contatos.indexOf(atual);
+        if (posicao >= 0) {
             contatos.set(posicao, novo);
             return true;
-        }else{
+        } else {
             return false;
         }
     }
 
-    //Remover um contato - DELETE
-    public boolean remover(Contato contato){
+    // Remover um contato - DELETE
+    public boolean remover(Contato contato) {
         return contatos.remove(contato);
     }
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Agenda agenda = (Agenda) o;
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        agenda agenda = (agenda) o;
         return Objects.equals(contatos, agenda.contatos);
     }
 
